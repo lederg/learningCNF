b9400ca38b9ec7ee035f1ec32b0aeb760fd58b4d
Markus N Rabe <markus.norman.rabe@gmail.com>
2018-02-02 17:26:57 -0800

    using cegar version of cadet to test satisfiability of QBF

--- cnf_tools.py
+++ cnf_tools.py
@@ -44,7 +44,7 @@ def is_sat(maxvar,clauses,universals=set()):
     # print(str(maxvar))
     # print(str(clauses))
     tool = 'cadet2.3.1' if len(universals) > 0 else 'picosat'
-    p = Popen([tool,'-v','1','--cegar'],stdout=PIPE,stdin=PIPE)
+    p = Popen([tool,'-v','1'],stdout=PIPE,stdin=PIPE)
     p.stdin.write(str.encode('p cnf {} {}\n'.format(maxvar,len(clauses))))
     if len(universals) > 0:
         p.stdin.write(str.encode('a'))

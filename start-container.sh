5c9584ae042c3bd138e109ed1ec0c03b929c63f2
gil <gillederman@gmail.com>
2018-07-08 16:38:27 -0700

    version 11 of docker image

--- start-container.sh
+++ start-container.sh
@@ -7,6 +7,6 @@ echo name is $1
 # echo docker-machine ssh $1 "sudo docker run --rm -v /efs/shared-code/learningCNF:/code gilled/learningcnf:v5 ${@:2}"
 # docker-machine ssh $1 "sudo docker run --rm -v /efs/shared-code/learningCNF:/code gilled/learningcnf:v5 ${@:2}"
 
-echo docker-machine ssh $1 "sudo docker run -d -v /home/ubuntu/learningCNF:/code -v /efs/runs_cadet/:/code/runs_cadet -v /home/ubuntu/synthetic_qbf_formulas:/code/data -v /efs/saved_models:/code/saved_models -v /efs/eps:/code/eps gilled/learningcnf:v5 ${@:2}"
-docker-machine ssh $1 "sudo docker run -d -v /home/ubuntu/learningCNF:/code -v /efs/runs_cadet/:/code/runs_cadet -v /home/ubuntu/synthetic_qbf_formulas:/code/data -v /efs/saved_models:/code/saved_models -v /efs/eps:/code/eps gilled/learningcnf:v5 ${@:2}"
+echo docker-machine ssh $1 "sudo docker run -d -v /home/ubuntu/learningCNF:/code -v /efs/runs_cadet/:/code/runs_cadet -v /home/ubuntu/synthetic_qbf_formulas:/code/data -v /efs/saved_models:/code/saved_models -v /efs/eps:/code/eps gilled/learningcnf:v11 ${@:2}"
+docker-machine ssh $1 "sudo docker run -d -v /home/ubuntu/learningCNF:/code -v /efs/runs_cadet/:/code/runs_cadet -v /home/ubuntu/synthetic_qbf_formulas:/code/data -v /efs/saved_models:/code/saved_models -v /efs/eps:/code/eps gilled/learningcnf:v11 ${@:2}"
 

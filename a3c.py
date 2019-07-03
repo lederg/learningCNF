ba0e34ae51e61d6e1b1664cd9b75e223bf5f578c
Gil <gilled@berkeley.edu>
2019-07-02 20:09:30 -0700

    save/update settings

--- a3c.py
+++ a3c.py
@@ -1,6 +1,7 @@
 import ipdb
 import cProfile
 import logging
+import json
 from sacred import Experiment
 from sacred.observers import MongoObserver
 from config import *
@@ -12,6 +13,12 @@ from settings import *
 
 @ex.automain
 def main():	
+	if ex.current_run.config['update_config']:
+		with open(ex.current_run.config['update_config']) as fp:
+			conf = json.load(fp)
+			for k in conf.keys():
+				ex.current_run.config[k] = conf[k]
+			ex.add_config(conf)
 	settings = CnfSettings(ex.current_run.config)
 	settings.hyperparameters['name'] = ex.current_run.experiment_info['name']
 	settings.hyperparameters['mp']=True
@@ -20,6 +27,9 @@ def main():
 		print('Adding mongodb observer')
 		ex.observers.append(MongoObserver.create(url=settings['mongo_host'],
                                          db_name=settings['mongo_dbname']))
+	if settings['save_config']:
+		with open('config_{}.json'.format(settings['name']), 'w') as fp:
+			json.dump(settings.hyperparameters, fp, indent=4)
 	from task_a3c import a3c_main
 	from task_parallel import parallel_main
 	from task_collectgrid import grid_main

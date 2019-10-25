7ea80abeb8f12ee43b42cfac4d24abad1089f09c
Ryan Brill <ryguy123@berkeley.edu>
2019-10-24 22:17:53 -0700

    aag_parser.py

--- qbf_data.py
+++ qbf_data.py
@@ -1,4 +1,5 @@
 from cnf_parser import *
+from aag_parser import read_qaiger
 from utils import *
 import numpy as np
 from functools import partial
@@ -373,7 +374,10 @@ class AagBase(object):
         else:
             return self.get_dense_adj_matrices(sample)
 
-    def get_sparse_adj_matrices(self):
+    def get_sparse_adj_matrix(self):
+        """
+        NOTE: I SUBTRACTED 1 FROM EACH NODE NUMBER
+        """
         sample = self.aag
         indices = []
         values = []
@@ -392,110 +396,28 @@ class AagBase(object):
         for i, ag in enumerate(sample['and_gates']):
             for l in ag[1:]:
                 val = 1 if l % 2 == 0 else -1
-                indices0.append(int(ag[0]/2))
-                indices1.append(int(l/2))
+                # NOTE: I SUBTRACTED 1 FROM EACH NODE NUMBER
+                indices0.append(int(ag[0]/2) - 1)
+                indices1.append(int(l/2) - 1)
                 values.append(val)
         i = torch.LongTensor([indices0, indices1])
-        v = torch.LongTensor(values)
-        return torch.sparse.FloatTensor(i, v)
-        #return torch.sparse.FloatTensor(i, v, torch.Size([n,n]))
-
+        v = torch.LongTensor(values)    
+        return torch.sparse_coo_tensor(indices = i, values = v, size=[n,n])#, size=[3,4])
     
+    def get_base_embeddings(self):
+        embs = np.zeros([self.num_vars,GROUND_DIM])
+        for i in (IDX_VAR_UNIVERSAL, IDX_VAR_EXISTENTIAL):
+            embs[:,i][np.where(self.var_types==i)]=1
+        return embs
+    
+    def get_clabels(self):
+        rc = np.ones(self.num_clauses)
+        rc[:len(self.qcnf['clauses'])]=0
+        return rc
+        
+    def get_geometric_data(self):
+        
+        edge_index = self.get_sparse_adj_matrix()
+        num_edges = self.aag['maxvar'] ** 2
+        edge_attr = torch.zeros([num_edges, 1]) # all original clauses
 
-def read_qaiger(filename):
-    """
-    read from a `.qaiger` file (which contains an `aag` circuit).
-    returns a dictionary..
-    """
-    maxvar = 0
-    num_inputs = 0
-    num_latches = 0
-    num_outputs = 0
-    num_and_gates = 0
-    inputs = []
-    latches = []
-    outputs = []
-    and_gates = []
-    input_symbols = []
-    output_symbols = []
-    avars = {}
-
-    with open(filename, 'r') as f:
-        while True:
-            a = f.readline()        # header
-            if a[0:3] == 'aag':
-                break
-        line = a.split(' ')         
-        # read the first line, like "aag 256 32 0 1 224"
-        maxvar = int(line[1])
-        num_inputs = int(line[2])
-        num_latches = int(line[3])
-        num_outputs = int(line[4])
-        num_and_gates = int(line[5])
-        
-        # initialize avars
-        for v in range(maxvar):
-            avars[v + 1] =  {'universal': 'Not yet implemented', 'and_gates': []}
-        
-        # read inputs
-        k = num_inputs
-        while k > 0 and a:
-            a = f.readline()
-            line = a.split()
-            inputs.append(int(line[0]))
-            k -= 1
-        
-        # ignore latches, for now
-        
-        # read outputs
-        k = num_outputs
-        while k > 0 and a:
-            a = f.readline()
-            line = a.split()
-            outputs.append(int(line[0]))
-            k -= 1
-        
-        # read and gates
-        k = num_and_gates
-        while k > 0 and a:
-            a = f.readline()
-            line = a.split()
-            and_gate = [int(line[0]), int(line[1]), int(line[2])]
-            and_gates.append(and_gate)
-            k -= 1
-            
-            # update avars
-            for l in and_gate:
-                v = int(l/2) # the variable v corresponding to the literal l
-                avars[v]['and_gates'].append(and_gate)
-            
-        # read input symbols
-        k = num_inputs
-        while k > 0 and a:
-            a = f.readline()
-            line = a.split()
-            input_symbols.append( ' '.join(line[1:]) )
-            k -= 1
-            
-        # read output symbols
-        k = num_outputs
-        while k > 0 and a:
-            a = f.readline()
-            line = a.split()
-            output_symbols.append( ' '.join(line[1:]) )
-            k -= 1
-        
-    return {'maxvar': maxvar,
-            'num_inputs': num_inputs,
-            'num_latches': num_latches,
-            'num_outputs': num_outputs,
-            'num_and_gates': num_and_gates,
-            'inputs': inputs,
-            'latches': latches,
-            'outputs': outputs,
-            'and_gates': and_gates,
-            'input_symbols': input_symbols,
-            'output_symbols': output_symbols,
-            #'avars': avars,
-            'fname': filename
-            }

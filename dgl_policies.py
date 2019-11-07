507fa34b2c938a19932ebdcff70bc8e302696450
Ryan Brill <ryguy123@berkeley.edu>
2019-11-07 15:42:42 -0800

    worked on AAGLayer

--- dgl_policies.py
+++ dgl_policies.py
@@ -58,22 +58,46 @@ class AAGLayer(nn.Module):
     self.settings = settings if settings else CnfSettings()
     self.activation = activation if activation else eval(self.settings['non_linearity'])
   
-  def forward(self, G, cnf_output):
+  def forward(self, G, feat_dict):
     # the input is a dictionary of node features for each type
-    Wh_af = self.weight['aag_forward'](cnf_output)
+    Wh_af = self.weight['aag_forward'](feat_dict['literal'])
     G.nodes['literal'].data['Wh_af'] = Wh_af
-    Wh_ab = self.weight['aag_backward'](cnf_output)
+    Wh_ab = self.weight['aag_backward'](feat_dict['literal'])
     G.nodes['literal'].data['Wh_ab'] = Wh_ab
-
-    G.send(G['aag_forward'].edges(), fn.copy_src('Wh_af', 'm_a'), etype='aag_forward')
-    G.send(G['aag_backward'].edges(), fn.copy_src('Wh_ab', 'm_a'), etype='aag_backward')
-    G.recv(G.nodes('literal'), fn.mean('m_a', 'h_a'), etype='aag_forward')
-    G.recv(G.nodes('literal'), fn.mean('m_a', 'h_a'), etype='aag_backward')
     
-    lembs = self.activation(G.nodes['literal'].data['h_a'])
-    return lembs
-    
+    G['aag_forward'].update_all(fn.copy_src('Wh_af', 'm_af'), fn.sum('m_af', 'h_af'))
+    G['aag_backward'].update_all(fn.copy_src('Wh_ab', 'm_ab'), fn.sum('m_ab', 'h_ab'))
+    lembs = G.nodes['literal'].data['h_af'] + G.nodes['literal'].data['h_ab']
 
+    # normalize by in_degree(v) + out_degree(v) for each literal node v
+    combined_degrees = G.out_degrees(etype="aag_forward") + G.out_degrees(etype="aag_backward")
+    combined_degrees[combined_degrees == 0] = 1
+    combined_degrees = combined_degrees.float()
+    lembs = ((lembs.T)/combined_degrees).T
+    G.nodes['literal'].data['h_a'] = lembs
+    
+    ##########################################################################
+    """
+    EXAMPLE:   files found in words_test_ryan_mini_1
+    10 literals            0,1,2,...,9
+    6 aag_forward_edges   (4,0) (4,2) (6,1) (6,4) (8,2) (8,7)
+    6 aag_backward_edges  (0,4) (2,4) (1,6) (4,6) (2,8) (7,8)
+    The following is what the literal embeddings should be,
+        given that we have already passed the cnf_output through a linear layer
+        to produce Wh_af, Wh_ab:
+    """
+    embs = torch.zeros(10, 5) 
+    embs[0] = Wh_af[4]
+    embs[1] = Wh_af[6]
+    embs[2] = (Wh_af[4] + Wh_af[8])/2
+    embs[4] = (Wh_af[6] + Wh_ab[0] + Wh_ab[2])/3
+    embs[6] = (Wh_ab[1] + Wh_ab[4])/2
+    embs[7] = Wh_af[8]
+    embs[8] = (Wh_ab[2] + Wh_ab[7])/2
+    ##########################################################################
+    
+    return self.activation(G.nodes['literal'].data['h_a'])
+   
 
 class CnfAagEncoder(nn.Module):
   def __init__(self, in_size, clause_size, out_size, settings=None, **kwargs):
@@ -94,131 +118,128 @@ class CnfAagEncoder(nn.Module):
     self.aag_layer = AAGLayer(2*self.vemb_dim,vemb_dim, **kwargs)
 
   def forward(self, G, feat_dict):
-
+    pass #FIXME
 
 
     
-class QbfNewEncoder(nn.Module):
-    def __init__(self, **kwargs):
-        super(QbfNewEncoder, self).__init__() 
-        self.settings = kwargs['settings'] if 'settings' in kwargs.keys() else CnfSettings()
-        self.debug = False
-        self.ground_dim = self.settings['ground_dim']
-        self.vlabel_dim = self.settings['vlabel_dim']
-        self.clabel_dim = self.settings['clabel_dim']
-        self.vemb_dim = self.settings['vemb_dim']
-        self.cemb_dim = self.settings['cemb_dim']
-        self.batch_size = self.settings['batch_size']
-        # self.embedding_dim = self.settings['embedding_dim']                
-        self.max_iters = self.settings['max_iters']        
-        self.non_linearity = eval(self.settings['non_linearity'])
-        W_L_params = []
-        B_L_params = []
-        W_C_params = []
-        B_C_params = []
-        # if self.settings['use_bn']:
-        self.vnorm_layers = nn.ModuleList([])
-        for i in range(self.max_iters):
-            W_L_params.append(nn.Parameter(self.settings.FloatTensor(self.cemb_dim,self.vlabel_dim+2*i*self.vemb_dim)))
-            B_L_params.append(nn.Parameter(self.settings.FloatTensor(self.cemb_dim)))
-            W_C_params.append(nn.Parameter(self.settings.FloatTensor(self.vemb_dim,self.clabel_dim+self.cemb_dim)))
-            B_C_params.append(nn.Parameter(self.settings.FloatTensor(self.vemb_dim)))
-            nn_init.normal_(W_L_params[i])
-            nn_init.normal_(B_L_params[i])        
-            nn_init.normal_(W_C_params[i])                
-            nn_init.normal_(B_C_params[i])
-            if self.settings['use_bn']:
-                self.vnorm_layers.append(nn.BatchNorm1d(self.vemb_dim))
-            elif self.settings['use_ln']:
-                self.vnorm_layers.append(nn.LayerNorm(self.vemb_dim))
-
-
-        if self.settings['use_gru']:
-            self.gru = GruOperator(embedding_dim=self.vemb_dim, settings=self.settings)
-
-        self.W_L_params = nn.ParameterList(W_L_params)
-        self.B_L_params = nn.ParameterList(B_L_params)
-        self.W_C_params = nn.ParameterList(W_C_params)
-        self.B_C_params = nn.ParameterList(B_C_params)
-        
-                    
-    def copy_from_encoder(self, other, freeze=False):
-        for i in range(len(other.W_L_params)):
-            self.W_L_params[i] = other.W_L_params[i]
-            self.B_L_params[i] = other.B_L_params[i]
-            self.W_C_params[i] = other.W_C_params[i]
-            self.B_C_params[i] = other.B_C_params[i]
-            if freeze:
-                self.W_L_params[i].requires_grad=False
-                self.B_L_params[i].requires_grad=False
-                self.W_C_params[i].requires_grad=False
-                self.B_C_params[i].requires_grad=False
-            if self.settings['use_bn']:
-                for i, layer in enumerate(other.vnorm_layers):
-                    self.vnorm_layers[i].load_state_dict(layer.state_dict())
-
-
-# vlabels are (vars,vlabel_dim)
-# clabels are sparse (clauses,clabel_dim)
-# cmat_pos and cmat_neg is the bs*v -> bs*c block-diagonal adjacency matrix 
-
-    def forward(self, vlabels, clabels, cmat_pos, cmat_neg, **kwargs):
-        # size = vlabels.size()
-        # bs = size[0]
-        # maxvars = size[1]
-        pos_vars = vlabels
-        neg_vars = vlabels
-        vmat_pos = cmat_pos.t()
-        vmat_neg = cmat_neg.t()
-        
-#        import ipdb
-#        ipdb.set_trace()
-
-
-        for t, p in enumerate(self.W_L_params):
-            # results is everything we computed so far, its precisely the correct input to W_L_t
-            av = (torch.mm(cmat_pos,pos_vars)+torch.mm(cmat_neg,neg_vars)).t()
-            c_t_pre = self.non_linearity(torch.mm(self.W_L_params[t],av).t() + self.B_L_params[t])
-            c_t = torch.cat([clabels,c_t_pre],dim=1)
-            pv = torch.mm(vmat_pos,c_t).t()
-            nv = torch.mm(vmat_neg,c_t).t()
-            pv_t_pre = self.non_linearity(torch.mm(self.W_C_params[t],pv).t() + self.B_C_params[t])
-            nv_t_pre = self.non_linearity(torch.mm(self.W_C_params[t],nv).t() + self.B_C_params[t])
-            if self.settings['use_bn'] or self.settings['use_ln']:
-                pv_t_pre = self.vnorm_layers[t](pv_t_pre.contiguous())
-                nv_t_pre = self.vnorm_layers[t](nv_t_pre.contiguous())            
-            # if bs>1:
-            #     Tracer()()            
-            pos_vars = torch.cat([pos_vars,pv_t_pre,nv_t_pre],dim=1)
-            neg_vars = torch.cat([neg_vars,nv_t_pre,pv_t_pre],dim=1)
-
-
-        return pos_vars, neg_vars   
+#class QbfNewEncoder(nn.Module):
+#    def __init__(self, **kwargs):
+#        super(QbfNewEncoder, self).__init__() 
+#        self.settings = kwargs['settings'] if 'settings' in kwargs.keys() else CnfSettings()
+#        self.debug = False
+#        self.ground_dim = self.settings['ground_dim']
+#        self.vlabel_dim = self.settings['vlabel_dim']
+#        self.clabel_dim = self.settings['clabel_dim']
+#        self.vemb_dim = self.settings['vemb_dim']
+#        self.cemb_dim = self.settings['cemb_dim']
+#        self.batch_size = self.settings['batch_size']
+#        # self.embedding_dim = self.settings['embedding_dim']                
+#        self.max_iters = self.settings['max_iters']        
+#        self.non_linearity = eval(self.settings['non_linearity'])
+#        W_L_params = []
+#        B_L_params = []
+#        W_C_params = []
+#        B_C_params = []
+#        # if self.settings['use_bn']:
+#        self.vnorm_layers = nn.ModuleList([])
+#        for i in range(self.max_iters):
+#            W_L_params.append(nn.Parameter(self.settings.FloatTensor(self.cemb_dim,self.vlabel_dim+2*i*self.vemb_dim)))
+#            B_L_params.append(nn.Parameter(self.settings.FloatTensor(self.cemb_dim)))
+#            W_C_params.append(nn.Parameter(self.settings.FloatTensor(self.vemb_dim,self.clabel_dim+self.cemb_dim)))
+#            B_C_params.append(nn.Parameter(self.settings.FloatTensor(self.vemb_dim)))
+#            nn_init.normal_(W_L_params[i])
+#            nn_init.normal_(B_L_params[i])        
+#            nn_init.normal_(W_C_params[i])                
+#            nn_init.normal_(B_C_params[i])
+#            if self.settings['use_bn']:
+#                self.vnorm_layers.append(nn.BatchNorm1d(self.vemb_dim))
+#            elif self.settings['use_ln']:
+#                self.vnorm_layers.append(nn.LayerNorm(self.vemb_dim))
+#
+#
+#        if self.settings['use_gru']:
+#            self.gru = GruOperator(embedding_dim=self.vemb_dim, settings=self.settings)
+#
+#        self.W_L_params = nn.ParameterList(W_L_params)
+#        self.B_L_params = nn.ParameterList(B_L_params)
+#        self.W_C_params = nn.ParameterList(W_C_params)
+#        self.B_C_params = nn.ParameterList(B_C_params)
+#        
+#                    
+#    def copy_from_encoder(self, other, freeze=False):
+#        for i in range(len(other.W_L_params)):
+#            self.W_L_params[i] = other.W_L_params[i]
+#            self.B_L_params[i] = other.B_L_params[i]
+#            self.W_C_params[i] = other.W_C_params[i]
+#            self.B_C_params[i] = other.B_C_params[i]
+#            if freeze:
+#                self.W_L_params[i].requires_grad=False
+#                self.B_L_params[i].requires_grad=False
+#                self.W_C_params[i].requires_grad=False
+#                self.B_C_params[i].requires_grad=False
+#            if self.settings['use_bn']:
+#                for i, layer in enumerate(other.vnorm_layers):
+#                    self.vnorm_layers[i].load_state_dict(layer.state_dict())
+#
+#
+## vlabels are (vars,vlabel_dim)
+## clabels are sparse (clauses,clabel_dim)
+## cmat_pos and cmat_neg is the bs*v -> bs*c block-diagonal adjacency matrix 
+#
+#    def forward(self, vlabels, clabels, cmat_pos, cmat_neg, **kwargs):
+#        # size = vlabels.size()
+#        # bs = size[0]
+#        # maxvars = size[1]
+#        pos_vars = vlabels
+#        neg_vars = vlabels
+#        vmat_pos = cmat_pos.t()
+#        vmat_neg = cmat_neg.t()
+#        
+##        import ipdb
+##        ipdb.set_trace()
+#
+#
+#        for t, p in enumerate(self.W_L_params):
+#            # results is everything we computed so far, its precisely the correct input to W_L_t
+#            av = (torch.mm(cmat_pos,pos_vars)+torch.mm(cmat_neg,neg_vars)).t()
+#            c_t_pre = self.non_linearity(torch.mm(self.W_L_params[t],av).t() + self.B_L_params[t])
+#            c_t = torch.cat([clabels,c_t_pre],dim=1)
+#            pv = torch.mm(vmat_pos,c_t).t()
+#            nv = torch.mm(vmat_neg,c_t).t()
+#            pv_t_pre = self.non_linearity(torch.mm(self.W_C_params[t],pv).t() + self.B_C_params[t])
+#            nv_t_pre = self.non_linearity(torch.mm(self.W_C_params[t],nv).t() + self.B_C_params[t])
+#            if self.settings['use_bn'] or self.settings['use_ln']:
+#                pv_t_pre = self.vnorm_layers[t](pv_t_pre.contiguous())
+#                nv_t_pre = self.vnorm_layers[t](nv_t_pre.contiguous())            
+#            # if bs>1:
+#            #     Tracer()()            
+#            pos_vars = torch.cat([pos_vars,pv_t_pre,nv_t_pre],dim=1)
+#            neg_vars = torch.cat([neg_vars,nv_t_pre,pv_t_pre],dim=1)
+#
+#
+#        return pos_vars, neg_vars   
     
 ###############################################################################
-"""Test CNFLayer and AAGLayer"""
+"""Test AAGLayer"""
 ###############################################################################
-#a = CombinedGraph1Base()
-#a.load_paired_files(aag_fname = './data/words_easy_train/words_0_SAT.qaiger', qcnf_fname = './data/words_easy_train/words_0_SAT.qaiger.qdimacs')
-#feat_dict = {
-#        'literal': a.G.nodes['literal'].data['lit_embs'],   # num_literals x 9 = 104 x 9
-#        'clause' : a.G.nodes['clause'].data['clause_embs']  # num_clauses  x 1 = 109 x 9
-#}
-#
-#in_size = feat_dict['literal'].shape[1]
-#clause_size = 11 #a.G.number_of_nodes('clause')
-#out_size = 107 #9 #FIXME??
-#C = CNFLayer(in_size, clause_size, out_size, activation=F.relu)
-#C_f = C(a.G, feat_dict) # shape        in_size x out_size = 
-#print(C_f)
-#print(C_f.shape)
-#
-#in_size = C_f.shape[1]
-#out_size = 82 #FIXME??
-#A = AAGLayer(in_size, out_size, activation=F.relu)
-#A_f = A(a.G, C_f)
-#print(A_f)
-#print(A_f.shape)
+a = CombinedGraph1Base()
+a.load_paired_files(aag_fname = './data/words_test_ryan_mini_1/w.qaiger', qcnf_fname = './data/words_test_ryan_mini_1/w.qaiger.qdimacs')
+G = a.G
+feat_dict = {
+        'literal': G.nodes['literal'].data['lit_embs'],   # num_literals x 9 = 104 x 9
+        'clause' : G.nodes['clause'].data['clause_embs']  # num_clauses  x 1 = 109 x 9
+}
 
+in_size = feat_dict['literal'].shape[1]
+clause_size = 11 
+out_size = 7
+C = CNFLayer(in_size, clause_size, out_size, activation=F.relu)
+C_f = C(G, feat_dict)  
+
+in_size = C_f.shape[1]
+out_size = 5 
+feat_dict = {'literal': C_f}
+A = AAGLayer(in_size, out_size, activation=F.relu)
+A_f = A(G, feat_dict)
 
 

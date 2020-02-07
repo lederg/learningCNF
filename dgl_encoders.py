fix max_iters bug, punch out lbd, add normalization

dgl_encoders.py
@@ -99,18 +99,36 @@ class DGLEncoder(nn.Module):
 class CNFEncoder(DGLEncoder):
   def __init__(self, settings=None, **kwargs):
     super(CNFEncoder, self).__init__(settings=settings, **kwargs)
+
+    if self.settings['cp_normalization'] == 'batch':
+      norm_class = nn.BatchNorm1d
+    elif self.settings['cp_normalization'] == 'layer':
+      norm_class = nn.LayerNorm
+    self.use_norm = self.settings['cp_normalization'] != None
     cnf_layer = CNFLayer
-    self.layers = nn.ModuleList([cnf_layer(self.vlabel_dim, self.cemb_dim, self.vemb_dim, activation=self.non_linearity, **kwargs)])
-    for i in range(1,self.max_iters):
-      self.layers.append(CNFLayer(2*self.vemb_dim, self.cemb_dim, self.vemb_dim, activation=self.non_linearity, **kwargs))
+    if self.max_iters > 0:
+      self.vnorm_layers = nn.ModuleList([norm_class(self.vemb_dim)]) if self.use_norm else nn.ModuleList([])
+      self.layers = nn.ModuleList([cnf_layer(self.vlabel_dim, self.cemb_dim, self.vemb_dim, activation=self.non_linearity, **kwargs)])
+      for i in range(1,self.max_iters):
+        self.layers.append(cnf_layer(2*self.vemb_dim, self.cemb_dim, self.vemb_dim, activation=self.non_linearity, **kwargs))
+        if self.use_norm:
+          self.vnorm_layers.append(norm_class(self.vemb_dim))
 
   def forward(self, G, feat_dict, **kwargs):    
     vlabels = feat_dict['literal']
-    embs = DGLEncoder.tie_literals(self.layers[0](G,feat_dict))
-    for i in range(1,self.max_iters):      
-      feat_dict['literal'] = embs
+    if self.max_iters == 0:
+      z = torch.zeros(1)
+      vz = z.expand(G.number_of_nodes('literal'),2*self.vemb_dim)
+      cz = z.expand(G.number_of_nodes('clause'),self.cemb_dim)
+      vembs = torch.cat([vz,vlabels],dim=1)
+      cembs = torch.cat([cz,feat_dict['clause']], dim=1)    
+      return vembs, cembs
+    for i in range(self.max_iters):
       pre_embs = self.layers[i](G, feat_dict)
+      if self.use_norm:
+          pre_embs = self.vnorm_layers[i](pre_embs)
       embs = DGLEncoder.tie_literals(pre_embs)
+      feat_dict['literal'] = embs
     vembs = torch.cat([embs,vlabels],dim=1)
     cembs = torch.cat([G.nodes['clause'].data['cembs'],feat_dict['clause']], dim=1)    
     return vembs, cembs

task_clause_prediction.py
@@ -156,8 +156,11 @@ def optimizer_creator(model, config):
 
 def data_creator(batch_size, config):
   settings = update_settings(config)
-  ds = CnfGNNDataset(settings['rl_train_data'], transform=transforms.Compose([SampleLearntClauses(10),CapActivity()]))
-  validate_ds = CnfGNNDataset(settings['rl_validation_data'], transforms.Compose([SampleLearntClauses(10),CapActivity()]))
+  trans = transforms.Compose([SampleLearntClauses(10),CapActivity()])
+  if not settings['cp_use_lbd']:
+    trans = transforms.Compose([ZeroLbd(),trans])
+  ds = CnfGNNDataset(settings['rl_train_data'], transform=trans)
+  validate_ds = CnfGNNDataset(settings['rl_validation_data'], trans)
   rc1 = torch.utils.data.DataLoader(ds, batch_size=batch_size, collate_fn=sat_collate_fn,shuffle=True, num_workers=3)
   rc2 = torch.utils.data.DataLoader(validate_ds, batch_size=batch_size, collate_fn=sat_collate_fn,shuffle=True, num_workers=2)
   return rc1, rc2
@@ -170,11 +173,12 @@ def clause_prediction_main():
     print('Running in ray cluster mode')
     ray.init(address=address, redis_password='blabla')
   else:
-    ray.init(num_cpus=settings['parallelism']+1)
+    ray.init()
+    # ray.init(num_cpus=settings['parallelism']+1)
   # criterion = torch.nn.CrossEntropyLoss()
-  # model = model_creator({})
-  # optimizer = optimizer_creator(model, {'lr': 1e-2})
-  # train_loader, validation_loader = data_creator(settings['batch_size'], {})
+  model = model_creator({})
+  optimizer = optimizer_creator(model, {'lr': 1e-2})
+  train_loader, validation_loader = data_creator(settings['batch_size'], {})
   cross_loss = lambda x: nn.CrossEntropyLoss()
   config = {    
     "model_creator": tune.function(model_creator),
@@ -190,9 +194,9 @@ def clause_prediction_main():
     "config": {
       "lr": settings['init_lr'],
       # "lr": tune.grid_search([1e-2,settings['init_lr']]),
-      "max_iters": tune.grid_search([0,1,2,3,4]),
-      "use_sum": tune.grid_search([True, False]),
-      "non_linearity": tune.grid_search(['torch.tanh', 'torch.relu']),
+      # "max_iters": tune.grid_search([0,1,2,3,4]),
+      # "use_sum": tune.grid_search([True, False]),
+      # "non_linearity": tune.grid_search(['torch.tanh', 'torch.relu']),
       "settings": settings.hyperparameters,
       },
   }
@@ -211,9 +215,10 @@ def clause_prediction_main():
   analysis = tune.run(
     ClausePredictionTrainable,
     name=settings['name'],
-    num_samples=2,
+    num_samples=settings['cp_num_samples'],
     # scheduler=pbt,
-    reuse_actors=True,        
+    reuse_actors=True,    
+    resources_per_trial={'cpu': 6, 'memory': 2**33},
     config=config,
     stop={"training_iteration": 40},
     verbose=1)
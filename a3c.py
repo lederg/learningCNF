43e73e48bfe72b5727aa03a17f76766b9170dd7e
Gil <gilled@berkeley.edu>
2020-04-21 16:17:28 -0700

    temp fix to experimental.sgd

--- a3c.py
+++ a3c.py
@@ -41,7 +41,7 @@ def main():
 	from task_pyro import pyro_main
 	from task_naive_es import naive_es_main
 	from task_sp import sp_main
-	from task_clause_prediction import clause_prediction_main
+	# from task_clause_prediction import clause_prediction_main
 	from task_rllib import rllib_main
 	from task_es import es_main
 	from task_es_eval import es_eval_main

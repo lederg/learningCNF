e7bd0b0767a6e609181fef59a3e42fdcfb414d09
Gil <gilled@berkeley.edu>
2020-05-09 21:09:50 -0700

    fix sudoku interface

---
+++ sudoku_models.py.py
@@ -0,0 +1,13 @@
+import torch
+import ipdb
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import init as nn_init
+
+class SudokuModel1(nn.Module):
+  def __init__(self, order=3):
+    super().__init__()
+
+
+  def forward(self, input_tensor):
+    pass
